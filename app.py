ef1fcc244d728f3c1956f66c8ad1aaa1173b787b
Cristian Ulises Ortega Padron <20001514@alumnos.utleon.edu.mx>
2023-03-10 18:26:17 -0600

    Clase Viernes 10/03/2023

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask import request
 from flask import url_for
 
@@ -29,6 +29,63 @@ def index():
 
     return render_template('index.html', form = frm_alummno)
 
+
+@app.route('/ABCompleto', methods=['GET', 'POST'])
+def ABCompleto():
+    frm_alummno = forms.UserForm(request.form)
+
+    alumnos = Alumnos.query.all()
+
+    return render_template('ABCompleto.html', form = frm_alummno, alumnos = alumnos)
+
+
+@app.route('/modificar', methods=['GET', 'POST'])
+def modificar():
+
+    frm_alummno = forms.UserForm(request.form)
+
+    if request.method == 'GET':
+
+        id = request.args.get('id')
+        
+        #SELECT * FROM alumnos WHERE id = id
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+
+        frm_alummno.id.data = alumno.id
+        frm_alummno.nombre.data = alumno.nombre
+        frm_alummno.apellidos.data = alumno.apellidos
+        frm_alummno.email.data = alumno.email
+
+    if request.method == 'POST':
+        
+        id = frm_alummno.id.data
+
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+
+        alumno.nombre = frm_alummno.nombre.data
+        alumno.apellidos = frm_alummno.apellidos.data
+        alumno.email = frm_alummno.email.data
+
+        db.session.add(alumno)
+        db.session.commit()
+
+        return redirect(url_for('ABCompleto'))
+
+    return render_template('modificar.html', form = frm_alummno)
+
+@app.route('/eliminar', methods=['GET'])
+def elimnar():
+    
+    if request.method == 'GET':
+        
+        id = request.args.get('id')
+
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+
+        db.session.delete(alumno)
+        db.session.commit()
+
+        return redirect(url_for('ABCompleto'))
 
 if __name__ == '__main__':
     csrf.init_app(app)
